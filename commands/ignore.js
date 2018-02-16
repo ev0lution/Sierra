4495c9f1992fdc3d2ffcab5dbbc0046985045e19
Andrew <nickfason32@gmail.com>
2018-02-15 19:16:03 -0600

    changed mysql node version and updated ignore.js a little bit

--- commands/ignore.js
+++ commands/ignore.js
@@ -1,12 +1,13 @@
+var mysql      = require('mysql');
+var pool  = mysql.createPool({
+  connectionLimit : 10,
+  host            : process.env.SQLHOST,
+  user            : process.env.SQLUSER,
+  password 		  : process.env.SQLPASS,
+  database        : 'sierra'
+});
+
 exports.run = (client, message, args) => {
-	const sqlite3 = require('sqlite3').verbose();
-	// Opens guild database
-	let db = new sqlite3.Database('./guilds.db', sqlite3.OPEN_READWRITE, (err) => {
-  	if (err) {
-  	  console.error(err.message);
-  	}
-	  console.log('Connected to the guilds database.');
-	});
 	message.channel.startTyping();
 	let args1 = args[0];
 	let channelMentions = message.mentions.channels.array();
@@ -17,8 +18,6 @@ exports.run = (client, message, args) => {
     	"color": 16713984
     	}});
     	message.channel.stopTyping();
-    	db.close();
-    	return;
 	} else
 	if (args1 !== "list") {
 		message.reply({"embed": {
@@ -27,35 +26,40 @@ exports.run = (client, message, args) => {
     	"color": 16713984
     	}});
     	message.channel.stopTyping();
-    	db.close();
-    	return;
 	} else
 	if (args1 === "list") {
 		let guildId = message.guild.id;
-		db.run("CREATE TABLE IF NOT EXISTS 'ignoredChannels' ('guildID'	INTEGER, 'ignoredIDs'	TEXT, PRIMARY KEY('guildID'));", (err, rows) => {
-			if (err) {
-				throw err;
-			}
-		});
-		let sql = `SELECT * FROM ignoredChannels WHERE guildID = ?`;
-    	db.all(sql, [guildId], (err, rows) => {
-		  if (err) {
-		  	message.channel.stopTyping();
-		    throw err;
+		let guildExistsInDB = "";
+    	pool.query("SELECT * FROM ignoredChannels WHERE guildID = ?", [guildId], function (error, results, fields) {
+		  if (error) throw error;
+		  let guildExistsInDB = results.length;
+		  if (guildExistsInDB === 0) {
+		  	pool.query("INSERT INTO ignoredChannels (guildID, ignoredIDs) VALUES (?, ?)", [guildId, ""], function (error) {
+		  		if (error) throw error;
+		  	});
+		  	message.reply({"embed": {
+		      "title": "Ignored Channels",
+		      "description": "There are no ignored channels on this server.",
+		      "color": 8311585
+		    }});
+		    message.channel.stopTyping();
 		  } else
-		  if (rows.length < 1) {
-		  	message.channel.stopTyping();
-		  	message.reply("<1");
+		  if (guildExistsInDB === 1) {
+		  	pool.query("SELECT ignoredIDs FROM ignoredChannels WHERE guildID = ?", [guildId], function (error, results) {
+		  		if (error) throw error;
+		  		message.reply(Array.isArray(results));
+		  		message.channel.stopTyping();
+			});
 		  }
 		});
-	} else
+		return;
+    }
 	if (!channelMentions || channelMentions.length < 1 || channelMentions === undefined)  {
 		message.reply({"embed": {
-    	"title": "Error",
-    	"description": "Please include a valid channel mention.\nDon't know how to mention channels? Check [here](https://github.com/NootSponge/Sierra/wiki/How-to-mention-a-channel).",
-    	"color": 16713984
-    	}});
+	 	"title": "Error",
+		"description": "Please include a valid channel mention.\nDon't know how to mention channels? Check [here](https://github.com/NootSponge/Sierra/wiki/How-to-mention-a-channel).",
+		"color": 16713984
+		}});
 	}
 	message.channel.stopTyping();
-	db.close();
 }
